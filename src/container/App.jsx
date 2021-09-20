a8a308c8503c33ed45518fd5ac147cd7843ee36d
Ohtico <viticogelvis5@gmail.com>
2021-09-19 21:33:08 -0500

    Ready

---
+++ src/container/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Calculadore} from '../components/Calculadore'
+
+const App = () => {
+    return (
+        <div>
+            <Calculadore/>
+        </div>
+    )
+}
+
+export  {App}
---
+++ src/style/SryledAll.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Contenedor = styled.div`
+    width: 23vw;
+    margin-top: 25vh;
+    display: flex;
+`;
+export const ContResult = styled.input`
+    width: 18vw;
+    font-size: 30px;
+    color: lightgray;
+    margin: 10px auto;
+`
